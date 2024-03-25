42126e0c903bc4b8386fa47060154cf07fb5e0ab
KennerEspinal <kennerespinalg@gmail.com>
2024-03-25 08:31:09 -0500

    HeroService Implemented

--- src/app/heroes/pages/list-page/list-page.component.ts
+++ src/app/heroes/pages/list-page/list-page.component.ts
@@ -1,10 +1,20 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { Hero } from '../../interfaces/hero.interface';
+import { HeroService } from '../../services/hero.service';
 
 @Component({
   selector: 'heroes-list-page',
   templateUrl: './list-page.component.html',
   styles: ``
 })
-export class ListPageComponent {
+export class ListPageComponent implements OnInit {
+  public heroes: Hero[] = [];
+
+  constructor(private hS: HeroService) { }
+
+  ngOnInit(): void {
+    this.hS.getHeroes().subscribe(heroes => this.heroes = heroes);
+  }
+
 
 }
