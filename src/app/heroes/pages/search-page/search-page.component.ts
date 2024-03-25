ee382c13f17556c0f125743fd6bc1a9ebe7b1357
KennerEspinal <kennerespinalg@gmail.com>
2024-03-25 10:45:48 -0500

    Hero New Page HTML

--- src/app/heroes/pages/search-page/search-page.component.ts
+++ src/app/heroes/pages/search-page/search-page.component.ts
@@ -1,4 +1,8 @@
 import { Component } from '@angular/core';
+import { FormControl } from '@angular/forms';
+import { Hero } from '../../interfaces/hero.interface';
+import { HeroService } from '../../services/hero.service';
+import { MatAutocompleteSelectedEvent } from '@angular/material/autocomplete';
 
 @Component({
   selector: 'heroes-search-page',
@@ -6,5 +10,26 @@ import { Component } from '@angular/core';
   styles: ``
 })
 export class SearchPageComponent {
+
+  searchInput = new FormControl('');
+  heroes: Hero[] = [];
+  selectedHero?: Hero;
+
+  constructor(private hS: HeroService) { }
+
+  searchHero() {
+    const value = this.searchInput.value || '';
+    this.hS.getSuggestions(value)
+      .subscribe(heroes => this.heroes = heroes);
+  }
+
+  onSelectedOption(event: MatAutocompleteSelectedEvent) {
+    if(!event.option.value) return this.selectedHero = undefined;
+
+    const hero: Hero = event.option.value;
+    this.searchInput.setValue(hero.superhero);
+
+    this.selectedHero = hero;
+  }
 
 }
